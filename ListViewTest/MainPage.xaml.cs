710e3ad0fb773ec7534f37e8e54ef6df2d95bfb3
v-liuson <v-liuson@MSDNUWPSERVER1>
2017-02-08 10:33:55 +0800

    Add project files.

---
+++ ListViewTest/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListViewTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ListView_DragItemsStarting(object sender, DragItemsStartingEventArgs e)
+        {
+            e.Data.RequestedOperation = DataPackageOperation.Move;
+        }
+
+        private void ListView_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void ListView_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Move;
+        }
+
+        private void ListView_Drop_1(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void MyList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var lv=sender as ListView;
+            var ob = (MyModelBase)lv.Items[lv.SelectedIndex];
+            Debug.WriteLine(ob.Name);
+            Debug.WriteLine(lv.SelectedIndex);
+        }
+    }
+}
---
+++ ListViewTest/MyViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewTest
+{
+    public class MyViewModel
+    {
+        public MyViewModel()
+        {
+            CreateList();
+            ListItems.CollectionChanged += Items_CollectionChanged;
+        }
+
+        private void CreateList()
+        {
+            ListItems.Add(new MyModelBase() { Name = "Prashant" });
+            ListItems.Add(new MyModelBase() { Name = "Ved" });
+            ListItems.Add(new MyModelBase() { Name = "Niraj" });
+            ListItems.Add(new MyModelBase() { Name = "Sanjeev" });
+            ListItems.Add(new MyModelBase() { Name = "Rahul" });
+            ListItems.Add(new MyModelBase() { Name = "Navneet" });
+            ListItems.Add(new MyModelBase() { Name = "Mayank" });
+            ListItems.Add(new MyModelBase() { Name = "Pramod" });
+            ListItems.Add(new MyModelBase() { Name = "Abhinav" });
+        }
+
+        private ObservableCollection<MyModelBase> _listItems = new ObservableCollection<MyModelBase>();
+        public ObservableCollection<MyModelBase> ListItems
+        {
+            get { return _listItems; }
+            set { _listItems = value; }
+        }
+
+        object m_ReorderItem;
+        int m_ReorderIndexFrom;
+        void Items_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Remove:
+                    m_ReorderItem = e.OldItems[0];
+                    m_ReorderIndexFrom = e.OldStartingIndex;
+                    break;
+                case NotifyCollectionChangedAction.Add:
+                    if (m_ReorderItem == null)
+                        return;
+                    var _ReorderIndexTo = e.NewStartingIndex;
+                    HandleReorder(m_ReorderItem, m_ReorderIndexFrom, _ReorderIndexTo);
+                    m_ReorderItem = null;
+                    break;
+            }
+            Debug.WriteLine(ListItems.ToArray());
+        }
+
+
+
+
+
+
+
+
+        void HandleReorder(object item, int indexFrom, int indexTo)
+        {
+            Debug.WriteLine("Reorder: {0}, From: {1}, To: {2}", item, indexFrom, indexTo);
+        }
+
+    }
+    public class MyModelBase
+    {
+        public string Name { get; set; }
+    }
+}
